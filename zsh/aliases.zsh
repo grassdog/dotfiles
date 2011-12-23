cb0cd35dd822d83a8a72d90509779e60b80e4e45
Ray Grasso <ray.grasso@gmail.com>
2011-12-24 09:15:08 +1100

    Add any alias

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -52,6 +52,16 @@ alias psu='ps auxw'         # Wide ps sorted by CPU usage
 alias tu='top -o cpu'       # cpu
 alias tm='top -o vsize'     # memory
 
+function any() {
+  emulate -L zsh
+  unsetopt KSH_ARRAYS
+  if [[ -z "$1" ]] ; then
+    echo "any - grep for process(es) by keyword" >&2
+    echo "Usage: any " >&2 ; return 1
+  else
+    ps xauwww | grep -i --color=auto "[${1[1]}]${1[2,-1]}"
+  fi
+}
 
 #########
 # Utils
