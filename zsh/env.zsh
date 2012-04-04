be5dbe5a42f4e229a85b5b17081a56bf91e54145
Ray Grasso <ray.grasso@gmail.com>
2012-04-04 15:52:29 +0800

    Update history on each command
    Also clean up the rest of the environment file.

--- zsh/env.zsh
+++ zsh/env.zsh
@@ -15,29 +15,27 @@ fi
 # Paths
 ##########
 
-export PATH="${HOME}/bin:${HOME}/.bin:/usr/local/bin:/usr/local/sbin:/usr/local/share/npm/bin:$PATH"
+export PATH="${HOME}/bin:${HOME}/.bin:/usr/local/bin:/usr/local/sbin:/usr/local/share/npm/bin:${PATH}"
 
-# Add Java 6 to my environment
-export PATH="$PATH:/System/Library/Frameworks/JavaVM.framework/Versions/1.6/Home/bin"
+# Add Java to my environment
+export PATH="${PATH}:/System/Library/Frameworks/JavaVM.framework/Versions/1.6/Home/bin"
 export JAVA_HOME="/System/Library/Frameworks/JavaVM.framework/Versions/1.6/Home"
-#export CATALINA_HOME=/Users/ray/tools/tomcat6
 
-# Adding an appropriate MANPATH variable for use with MacPorts.
-export MANPATH=/opt/local/share/man:$MANPATH
-
+# Adding Homebrew man path
+export MANPATH="/usr/local/share/man:${MANPATH}"
 
 ##########
 # History
 ##########
 
-setopt hist_ignore_dups     # ignore duplication command history list
-setopt HIST_IGNORE_SPACE
-setopt APPEND_HISTORY       # write history only when closing
+setopt HIST_IGNORE_DUPS     # Ignore adjacent duplication command history list
+setopt HIST_IGNORE_SPACE    # Don't store commands with a leading space into history
+setopt INC_APPEND_HISTORY   # write history on each command
+setopt SHARE_HISTORY        # share history across sessions
 setopt EXTENDED_HISTORY     # add more info
-#setopt share_history       # share command history data
-HISTFILE=~/.zsh_history
-SAVEHIST=1000000
-HISTSIZE=1000000
+export HISTFILE=~/.zsh_history
+export SAVEHIST=1000000
+export HISTSIZE=1000000
 
 ##########
 # Prompt
@@ -52,36 +50,23 @@ PROMPT='%{$fg[green]%}$(date "+%a %H:%M:%S") %{$fg[cyan]%}%n@%m:%{$reset_color%}
 
 
 #################
-# Configure zsh
+# Corrections
 #################
 
-# I don't want corrections
-setopt NOCORRECT
-setopt NOCORRECTALL # Only want commands not args
-setopt CLOBBER      # Allow redirect to clobber files
+setopt NOCORRECT    # I don't want corrections
+setopt NOCORRECTALL # Only correct commands not args
 
-setopt autopushd pushdminus pushdtohome autocd pushdignoredups
+setopt AUTOPUSHD PUSHDMINUS PUSHDTOHOME AUTOCD PUSHDIGNOREDUPS
 
-
-# setopt PRINT_EXIT_VALUE
-
-# Other tabbing options
-# setopt NO_AUTO_MENU
-# setopt BASH_AUTO_LIST
-
-###############
-# URL quoting
-###############
-
-# Quote pasted URLs
+# Correctly escape pasted URLs
 autoload url-quote-magic
 zle -N self-insert url-quote-magic
 
-# Allow for file truncation
-NULLCMD=:
+setopt CLOBBER      # Allow redirect to clobber files
+export NULLCMD=:           # Allow for file truncation
 
-REPORTTIME=10 # Show elapsed time if command took more than X seconds
-LISTMAX=0 # ask to complete if top of list would scroll off screen
+export REPORTTIME=10       # Show elapsed time if command took more than X seconds
+export LISTMAX=0           # Ask to complete if top of list would scroll off screen
 
 ###############
 # Completions
@@ -93,8 +78,7 @@ compinit
 fpath=($HOME/.zsh/Completion $fpath)
 
 # Make CTRL-W delete after other chars, not just spaces
-#WORDCHARS=${WORDCHARS//[&=\/;\!#%\{]}
-WORDCHARS='*?[]~=&;!#$%^(){}'
+export WORDCHARS='*?[]~=&;!#$%^(){}'
 
 # Emacs key bindings
 bindkey -e
@@ -112,12 +96,12 @@ bindkey "\e[F" end-of-line
 bindkey "\eOH" beginning-of-line
 bindkey "\eOF" end-of-line
 bindkey "\eOd" backward-word
-bindkey "\eOc" forward-word 
+bindkey "\eOc" forward-word
 
-# mappings for Ctrl/Option-left-arrow and Ctrl/Option-right-arrow for word moving
-#bindkey "\e\e[C"  forward-word
-#bindkey "\e\e[D"  backward-word
 
+# Add RVM to PATH for scripting
+PATH=$PATH:$HOME/.rvm/bin
 
 # Add rvm to my environment
 [[ -s $HOME/.rvm/scripts/rvm ]] && source $HOME/.rvm/scripts/rvm
+
