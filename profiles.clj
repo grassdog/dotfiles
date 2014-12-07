56ac683f5351984623fa9e729abc7d087a20e635
Ray Grasso <ray.grasso@gmail.com>
2014-12-07 12:55:47 +0800

    Add linting and style checks to profiles.clj

--- profiles.clj
+++ profiles.clj
@@ -1,6 +1,9 @@
 {:user
  {:dev
   {:dependencies [[alembic "0.2.1"]]}
-   :plugins [[cider/cider-nrepl "0.6.1-SNAPSHOT"]]}}
+   :plugins [
+             [cider/cider-nrepl "0.6.1-SNAPSHOT"]
+             [jonase/eastwood "0.2.0"]
+             [lein-kibit "0.0.8"]]}}
 
 
