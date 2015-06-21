3f4935c38536789dff2c7d2bda4f0ff0cafd5592
Ray Grasso <ray.grasso@gmail.com>
2015-06-21 17:06:14 +0800

    Fix up line movement

--- emacs.d/grass/grass-evil.el
+++ emacs.d/grass/grass-evil.el
@@ -12,6 +12,14 @@
 (require 'evil-matchit)
 (global-evil-matchit-mode 1)
 
+;; Make movement keys work like they should
+(define-key evil-normal-state-map (kbd "<remap> <evil-next-line>") 'evil-next-visual-line)
+(define-key evil-normal-state-map (kbd "<remap> <evil-previous-line>") 'evil-previous-visual-line)
+(define-key evil-motion-state-map (kbd "<remap> <evil-next-line>") 'evil-next-visual-line)
+(define-key evil-motion-state-map (kbd "<remap> <evil-previous-line>") 'evil-previous-visual-line)
+; Make horizontal movement cross lines
+(setq-default evil-cross-lines t)
+
 (require 'evil)
 (evil-mode t)
 
