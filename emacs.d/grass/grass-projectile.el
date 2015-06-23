e6ebfb79e719af1036328e30df2bf65b716108d9
Ray Grasso <ray.grasso@gmail.com>
2015-06-23 12:27:32 +0800

    Use proper helm projectile functions

--- emacs.d/grass/grass-projectile.el
+++ emacs.d/grass/grass-projectile.el
@@ -4,10 +4,15 @@
 (setq projectile-enable-caching t)
 (projectile-global-mode t)
 
+(add-to-list 'projectile-globally-ignored-directories "gems")
+(add-to-list 'projectile-globally-ignored-directories "node_modules")
+
 (setq projectile-completion-system 'helm)
+(setq helm-projectile-fuzzy-match t)
 (helm-projectile-on)
 
-(evil-leader/set-key "p" 'projectile-find-file)
+(evil-leader/set-key "p" 'helm-projectile)
 (evil-leader/set-key "a" 'helm-projectile-ag)
+(evil-leader/set-key "s" 'helm-projectile-switch-project)
 
 (provide 'grass-projectile)
