d176e4cc7858ea0b1b05c807e20b5150ea71f9f5
Ray Grasso <ray.grasso@gmail.com>
2015-06-23 12:26:06 +0800

    Maintain visual selection on indent/outdent

--- emacs.d/grass/grass-evil.el
+++ emacs.d/grass/grass-evil.el
@@ -34,4 +34,22 @@
 (define-key minibuffer-local-must-match-map [escape] 'minibuffer-keyboard-quit)
 (define-key minibuffer-local-isearch-map [escape] 'minibuffer-keyboard-quit)
 
+;; Overload shifts so that they don't lose the selection
+(define-key evil-visual-state-map (kbd ">") 'grass/evil-shift-right-visual)
+(define-key evil-visual-state-map (kbd "<") 'grass/evil-shift-left-visual)
+(define-key evil-visual-state-map [tab] 'grass/evil-shift-right-visual)
+(define-key evil-visual-state-map [S-tab] 'grass/evil-shift-left-visual)
+
+(defun grass/evil-shift-left-visual ()
+  (interactive)
+  (evil-shift-left (region-beginning) (region-end))
+  (evil-normal-state)
+  (evil-visual-restore))
+
+(defun grass/evil-shift-right-visual ()
+  (interactive)
+  (evil-shift-right (region-beginning) (region-end))
+  (evil-normal-state)
+  (evil-visual-restore))
+
 (provide 'grass-evil)
