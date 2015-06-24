5cbd44265cdd1d6edc842f4781fbe23fdc8ee4b1
Ray Grasso <ray.grasso@gmail.com>
2015-06-24 22:02:07 +0800

    Set some lisp defaults

--- emacs.d/grass/grass-lisp.el
+++ emacs.d/grass/grass-lisp.el
@@ -1,2 +1,12 @@
+(defun grass/lisp-coding-defaults ()
+  (paredit-mode +1)
+  (rainbow-delimiters-mode +1))
+
+(add-hook 'lisp-mode-hook 'grass/lisp-coding-defaults)
+(add-hook 'elisp-mode-hook 'grass/lisp-coding-defaults)
+(add-hook 'scheme-mode-hook 'grass/lisp-coding-defaults)
+
+; TODO Get interactive elisp working
+; (add-hook 'ielm-mode-hook 'grass/lisp-coding-defaults)
 
 (provide 'grass-lisp)
