9260b2cfe7e2f7bfb10678e8f4dac228601a6815
Ray Grasso <ray.grasso@gmail.com>
2015-06-24 15:50:23 +0800

    Map up in dired to a nicer key

--- emacs.d/grass/grass-keybindings.el
+++ emacs.d/grass/grass-keybindings.el
@@ -3,11 +3,10 @@
 (evil-leader/set-key "e" 'pp-eval-last-sexp)
 
 ;; Move lines
-;; TODO Apply this in a hook
 (global-set-key (kbd "<C-S-up>") 'grass/move-line-up)
 (global-set-key (kbd "<C-S-down>")  'grass/move-line-down)
 
-;; Create file in dired
-(evil-leader/set-key-for-mode 'dired-mode "c" 'find-file)
+(add-hook 'dired-mode-hook
+          (lambda () (local-set-key (kbd "U") 'dired-up-directory)))
 
 (provide 'grass-keybindings)
