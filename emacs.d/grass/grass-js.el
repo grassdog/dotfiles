cd1ac767f6997b57e2c8ceee08c6858e10674f4e
Ray Grasso <ray.grasso@gmail.com>
2015-06-25 12:13:21 +0800

    Use js2 for json

--- emacs.d/grass/grass-js.el
+++ emacs.d/grass/grass-js.el
@@ -1,6 +1,7 @@
 (require 'js2-mode)
 
 (add-to-list 'auto-mode-alist '("\\.js\\'"    . js2-mode))
+(add-to-list 'auto-mode-alist '("\\.json\\'"    . js2-mode))
 (add-to-list 'interpreter-mode-alist '("node" . js2-mode))
 
 (add-hook 'js2-mode-hook
