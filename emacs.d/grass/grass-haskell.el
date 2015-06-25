d37a1075d3d05761caf982725506dd8671f63403
Ray Grasso <ray.grasso@gmail.com>
2015-06-25 16:53:10 +0800

    Tweak haskell

--- emacs.d/grass/grass-haskell.el
+++ emacs.d/grass/grass-haskell.el
@@ -1,25 +1,32 @@
-(require 'haskell-mode)
+; (require 'haskell-mode)
 
 ;; Install some useful packages so this all works
 ;; cabal update && cabal install happy hasktags stylish-haskell present ghc-mod hlint
 
-(add-hook 'haskell-mode-hook 'turn-on-hi2)
-
-(eval-after-load 'haskell-mode
-          '(define-key haskell-mode-map [f8] 'haskell-navigate-imports))
-
+; Make Emacs look in Cabal directory for binaries
 (let ((my-cabal-path (expand-file-name "~/.cabal/bin")))
   (setenv "PATH" (concat my-cabal-path path-separator (getenv "PATH")))
   (add-to-list 'exec-path my-cabal-path))
 
-;; Use cabal for repl
-(custom-set-variables '(haskell-tags-on-save t) '(haskell-process-type 'cabal-repl))
+;; Use hi2 for indentation
+(require 'hi2)
+(add-hook 'haskell-mode-hook 'turn-on-hi2)
+
+; Add F8 key combination for going to imports block
+(eval-after-load 'haskell-mode
+  '(define-key haskell-mode-map [f8] 'haskell-navigate-imports))
 
 (custom-set-variables
-  '(haskell-process-suggest-remove-import-lines t)
-  '(haskell-process-auto-import-loaded-modules t)
-  '(haskell-process-log t))
+ ; Set up hasktags
+ '(haskell-tags-on-save t)
+ ; Set up interactive mode
+ '(haskell-process-auto-import-loaded-modules t)
+ '(haskell-process-log t)
+ '(haskell-process-suggest-remove-import-lines t)
+ ; Set interpreter to be "cabal repl"
+ '(haskell-process-type 'cabal-repl))
 
+; Add key combinations for interactive haskell-mode
 (eval-after-load 'haskell-mode '(progn
   (define-key haskell-mode-map (kbd "C-c C-l") 'haskell-process-load-or-reload)
   (define-key haskell-mode-map (kbd "C-c C-z") 'haskell-interactive-switch)
@@ -28,7 +35,6 @@
   (define-key haskell-mode-map (kbd "C-c C-n C-c") 'haskell-process-cabal-build)
   (define-key haskell-mode-map (kbd "C-c C-n c") 'haskell-process-cabal)
   (define-key haskell-mode-map (kbd "SPC") 'haskell-mode-contextual-space)))
-
 (eval-after-load 'haskell-cabal '(progn
   (define-key haskell-cabal-mode-map (kbd "C-c C-z") 'haskell-interactive-switch)
   (define-key haskell-cabal-mode-map (kbd "C-c C-k") 'haskell-interactive-mode-clear)
@@ -43,12 +49,12 @@
 (require 'rainbow-delimiters)
 (add-hook 'haskell-mode-hook #'rainbow-delimiters-mode)
 
-; (require 'ghc)
-; (autoload 'ghc-init "ghc" nil t)
-; (autoload 'ghc-debug "ghc" nil t)
-; (add-hook 'haskell-mode-hook (lambda () (ghc-init)))
+(require 'ghc)
+(autoload 'ghc-init "ghc" nil t)
+(autoload 'ghc-debug "ghc" nil t)
+(add-hook 'haskell-mode-hook (lambda () (ghc-init)))
 
-; (add-to-list 'company-backends 'company-ghc)
-; (custom-set-variables '(company-ghc-show-info t))
+(add-to-list 'company-backends 'company-ghc)
+(custom-set-variables '(company-ghc-show-info t))
 
 (provide 'grass-haskell)
