c8bde63dde89f9c5eb77badc943c82bb33c213bb
Ray Grasso <ray.grasso@gmail.com>
2015-06-28 23:14:34 +0800

    Tweak Ruby config

--- emacs.d/grass/grass-ruby.el
+++ emacs.d/grass/grass-ruby.el
@@ -5,36 +5,35 @@
 (use-package ruby-tools
   :ensure t)
 
+(use-package enh-ruby-mode
+  :ensure t
+  :mode ("\\.rb\\'"  "\\.rake\\'" "Rakefile\\'" "\\.gemspec\\'" "\\.ru\\'" "Gemfile\\'" "Guardfile\\'" "Capfile\\'" "\\.thor\\'" "Thorfile\\'" "Vagrantfile\\'")
+  :interpreter "ruby"
+  :init
+  (add-hook 'enh-ruby-mode-hook
+    (lambda ()
+      ;; turn off the annoying input echo in irb
+      (setq comint-process-echoes t)
+      (ruby-end-mode +1)
+      (ruby-tools-mode +1)
+      (setq evil-shift-width ruby-indent-level)
+      ;; CamelCase aware editing operations
+      (subword-mode +1))))
 
-;; TODO Get these working
-;; enh-ruby-mode
-;; rspec-mode
-;; chruby
+(use-package inf-ruby
+  :ensure t
+  :init
+  (add-hook 'enh-ruby-mode-hook 'inf-ruby-minor-mode))
 
-;; Rake files are ruby, too, as are gemspecs, rackup files, and gemfiles.
-(add-to-list 'auto-mode-alist '("\\.rake\\'" . ruby-mode))
-(add-to-list 'auto-mode-alist '("Rakefile\\'" . ruby-mode))
-(add-to-list 'auto-mode-alist '("\\.gemspec\\'" . ruby-mode))
-(add-to-list 'auto-mode-alist '("\\.ru\\'" . ruby-mode))
-(add-to-list 'auto-mode-alist '("Gemfile\\'" . ruby-mode))
-(add-to-list 'auto-mode-alist '("Guardfile\\'" . ruby-mode))
-(add-to-list 'auto-mode-alist '("Capfile\\'" . ruby-mode))
-(add-to-list 'auto-mode-alist '("\\.thor\\'" . ruby-mode))
-(add-to-list 'auto-mode-alist '("Thorfile\\'" . ruby-mode))
-(add-to-list 'auto-mode-alist '("Vagrantfile\\'" . ruby-mode))
+(use-package chruby
+  :ensure t
+  :init
+  (chruby))
+
+(use-package rspec-mode
+  :ensure t)
 
 ;; We never want to edit Rubinius bytecode
 (add-to-list 'completion-ignored-extensions ".rbc")
-
-(add-hook 'ruby-mode-hook
- (lambda ()
-  ;; turn off the annoying input echo in irb
-  (setq comint-process-echoes t)
-  (ruby-block-mode t)
-  (ruby-end-mode +1)
-  (ruby-tools-mode +1)
-  (setq evil-shift-width ruby-indent-level)
-  ;; CamelCase aware editing operations
-  (subword-mode +1)))
 
 (provide 'grass-ruby)
