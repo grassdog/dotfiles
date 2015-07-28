991afe8efba5e37b77c75725e084857a5558732c
Ray Grasso <ray.grasso@gmail.com>
2015-07-28 15:55:01 +0800

    Update cider and add lein-midje to profile

--- profiles.clj
+++ profiles.clj
@@ -1,8 +1,8 @@
 {:user
  {:dev
   {:dependencies [[alembic "0.2.1"]]}
-   :plugins [
-             [cider/cider-nrepl "0.6.1-SNAPSHOT"]
+   :plugins [[lein-midje "3.1.3"]
+             [cider/cider-nrepl "0.9.1"]
              [jonase/eastwood "0.2.0"]
              [lein-kibit "0.0.8"]]}}
 
