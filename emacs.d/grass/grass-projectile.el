6ded7447e11663ae2fe8dbd89b1815bd54618be6
Ray Grasso <ray.grasso@gmail.com>
2015-08-04 12:01:47 +0800

    Bind C-, f to helm-projectile cause it's nicer

--- emacs.d/grass/grass-projectile.el
+++ emacs.d/grass/grass-projectile.el
@@ -15,7 +15,7 @@
     (add-to-list 'projectile-globally-ignored-directories "node_modules")
     (add-to-list 'projectile-globally-ignored-directories "dist")
 
-    (global-set-key (kbd "C-, f") 'projectile-find-file)
+    (global-set-key (kbd "C-, f") 'helm-projectile)
     (helm-projectile-on)))
 
 (provide 'grass-projectile)
