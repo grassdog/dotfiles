8cb18bf80bbfccf4323626fe7d77b9402fe79172
Ray Grasso <ray.grasso@gmail.com>
2015-08-06 10:03:32 +0800

    Ignore elpa dir in projectile

--- emacs.d/grass/grass-projectile.el
+++ emacs.d/grass/grass-projectile.el
@@ -14,6 +14,7 @@
     (add-to-list 'projectile-globally-ignored-directories "gems")
     (add-to-list 'projectile-globally-ignored-directories "node_modules")
     (add-to-list 'projectile-globally-ignored-directories "dist")
+    (add-to-list 'projectile-globally-ignored-directories "elpa")
 
     (global-set-key (kbd "C-, C-p") 'helm-projectile)
     (global-set-key (kbd "C-, p") 'helm-projectile)
