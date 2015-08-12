1a5eff9e82a0fffad6533d0cb6aeeecd79b060f6
Ray Grasso <ray.grasso@gmail.com>
2015-08-12 19:18:31 +0800

    Set hl-sexp-face for terminal

--- emacs.d/custom.el
+++ emacs.d/custom.el
@@ -1,14 +1,25 @@
 
-;; Override background colour to work better with Solarized
 (custom-set-faces
   ;; custom-set-faces was added by Custom.
   ;; If you edit it by hand, you could mess it up, so be careful.
   ;; Your init file should contain only one such instance.
   ;; If there is more than one, they won't work right.
-  '(hl-sexp-face ((t (:background "#073642"))))
-  '(tabbar-selected-modified ((t :inherit tabbar-selected)))
-  '(tabbar-unselected-modified ((t :inherit tabbar-unselected)))
-  '(tabbar-unselected-modified ((t :inherit tabbar-unselected))))
+)
+
+(if (display-graphic-p)
+  (custom-set-faces
+    ;; custom-set-faces was added by Custom.
+    ;; If you edit it by hand, you could mess it up, so be careful.
+    ;; Your init file should contain only one such instance.
+    ;; If there is more than one, they won't work right.
+  '(hl-sexp-face ((t (:background "#073642")))))
+
+  (custom-set-faces
+    ;; custom-set-faces was added by Custom.
+    ;; If you edit it by hand, you could mess it up, so be careful.
+    ;; Your init file should contain only one such instance.
+    ;; If there is more than one, they won't work right.
+  '(hl-sexp-face ((t (:background "#616161"))))))
 
 (custom-set-variables
   ; Set up hasktags
