a78b04f074784fa88002c9fd43fa1f3828b4ce73
Ray Grasso <ray.grasso@gmail.com>
2015-08-12 21:12:28 +0800

    Add flycheck to js2mode

--- emacs.d/grass/grass-js.el
+++ emacs.d/grass/grass-js.el
@@ -8,9 +8,14 @@
     (add-hook 'js2-mode-hook
     (lambda ()
         (setq mode-name "JS2")
-        (setq js2-global-externs '("require" "module" "jest" "jasmine"
-                                    "it" "expect" "describe" "beforeEach"))
+        (setq js2-global-externs '("module" "require" "buster" "jestsinon" "jasmine" "assert"
+                                   "it" "expect" "describe" "beforeEach"
+                                   "refute" "setTimeout" "clearTimeout" "setInterval"
+                                   "clearInterval" "location" "__dirname" "console" "JSON"))
         (setq evil-shift-width js-indent-level)
+
+        (setq js2-show-parse-errors nil) ;; Rely on flycheck instead... 
+        (flycheck-mode t)
         (global-set-key (kbd "C-, f") 'web-beautify-js)
         (js2-imenu-extras-mode +1))))
 
