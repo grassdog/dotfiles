02098fe9635f0702e7ef86e589afa7638c7e75d0
Ray Grasso <ray.grasso@gmail.com>
2015-08-18 20:52:55 +0800

    Ignore .keep files

--- emacs.d/grass/grass-projectile.el
+++ emacs.d/grass/grass-projectile.el
@@ -15,6 +15,7 @@
     (add-to-list 'projectile-globally-ignored-directories "node_modules")
     (add-to-list 'projectile-globally-ignored-directories "dist")
     (add-to-list 'projectile-globally-ignored-directories "elpa")
+    (add-to-list 'projectile-globally-ignored-files ".keep")
 
     (global-set-key (kbd "C-, C-p") 'helm-projectile)
     (global-set-key (kbd "C-, p") 'helm-projectile)
