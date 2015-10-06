a0d3a9dfaf39c7e61737939110070c1f2dc117d4
Ray Grasso <ray.grasso@gmail.com>
2015-10-06 10:58:46 +0800

    Update helm config

--- emacs.d/grass/grass-helm.el
+++ emacs.d/grass/grass-helm.el
@@ -4,18 +4,27 @@
   :diminish helm-mode
   :bind (("C-, o" . helm-buffers-list)
          ("C-, r" . helm-recentf)
+         ("C-, C-f" . helm-find-files)
          ("M-x" . helm-M-x))
   :config
     (require 'helm-config)
+
+    (define-key helm-map (kbd "C-, l")  'helm-select-action)
+    (define-key helm-map (kbd "<tab>") 'helm-execute-persistent-action)
+    ;; Make TAB works in terminal
+    (define-key helm-map (kbd "C-i") 'helm-execute-persistent-action)
+
     ;; Show full buffer names please
     (setq helm-buffer-max-length 40)
     (setq helm-buffers-fuzzy-matching t)
-    (setq helm-buffers-fuzzy-matching t)
     (setq helm-M-x-fuzzy-match t)
+    (setq helm-recentf-fuzzy-match t)
+    (setq helm-buffers-fuzzy-matching t)
     (setq helm-split-window-in-side-p t)
 
     (use-package helm-ag
       :commands helm-ag
+      ;; Prepopulate search with the symbol under point
       ;; (setq helm-ag-insert-at-point 'symbol)
       )
 
