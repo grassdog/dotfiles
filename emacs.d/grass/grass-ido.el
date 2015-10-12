3871d6c87625004b55361df6773a558de4ed2feb
Ray Grasso <ray.grasso@gmail.com>
2015-10-12 20:48:37 +0800

    Clean up ido config with use-package

--- emacs.d/grass/grass-ido.el
+++ emacs.d/grass/grass-ido.el
@@ -1,46 +1,52 @@
-;; Better suggestions
-(setq ido-enable-prefix nil
-      ido-enable-flex-matching t
-      ido-create-new-buffer 'always
-      ido-use-filename-at-point 'guess
-      ido-max-prospects 10
-      ido-save-directory-list-file (expand-file-name "ido.hist" grass/savefile-dir)
-      ido-default-file-method 'selected-window
-      ido-auto-merge-work-directories-length -1
-      org-completion-use-ido t)
 
-;; Bind `~` to go to homedir when in ido-find-file;
-;; http://whattheemacsd.com/setup-ido.el-02.html
-(add-hook 'ido-setup-hook
- (lambda ()
-   ;; Go straight home
-   (define-key ido-file-completion-map
-     (kbd "~")
-     (lambda ()
-       (interactive)
-       (if (looking-back "/")
-           (insert "~/")
-         (call-interactively 'self-insert-command))))))
-
-(require 'ido)
-(ido-mode +1)
-
-(use-package ido-ubiquitous
+(use-package ido
+  :config
+  ;; Better suggestions
+  (setq ido-enable-prefix nil
+        ido-enable-flex-matching t
+        ido-create-new-buffer 'always
+        ido-use-filename-at-point 'guess
+        ido-max-prospects 10
+        ido-save-directory-list-file (expand-file-name "ido.hist" grass/savefile-dir)
+        ido-default-file-method 'selected-window
+        ido-auto-merge-work-directories-length -1
+        org-completion-use-ido t)
   :init
-    (ido-ubiquitous-mode +1))
+  (progn
+    (setq ido-enable-flex-matching t)
+    (setq ido-everywhere t)
+    (ido-mode t)
 
-(use-package flx-ido
-  :config
-    ;; disable ido faces to see flx highlights
-    (setq ido-use-faces nil)
-    ;; Smarter fuzzy matching for ido
-    (flx-ido-mode +1))
+    ;; Bind `~` to go to homedir when in ido-find-file;
+    ;; http://whattheemacsd.com/setup-ido.el-02.html
+    (add-hook 'ido-setup-hook
+    (lambda ()
+      ;; Go straight home
+      (define-key ido-file-completion-map
+        (kbd "~")
+        (lambda ()
+          (interactive)
+          (if (looking-back "/")
+              (insert "~/")
+            (call-interactively 'self-insert-command))))))
 
-(use-package ido-ubiquitous
-  :config
-    (require 'ido-vertical-mode)
-    ;; Allow up and down arrow to work for navigation
-    (setq ido-vertical-define-keys 'C-n-C-p-up-down-left-right)
-    (ido-vertical-mode 1))
+    (use-package flx-ido
+      :config
+      ;; disable ido faces to see flx highlights
+      (setq ido-use-faces nil)
+      ;; Smarter fuzzy matching for ido
+      (flx-ido-mode +1))
+
+    (use-package ido-ubiquitous
+      :disabled
+      :init (ido-ubiquitous-mode))
+
+    (use-package ido-vertical-mode
+      :init
+      (ido-vertical-mode 1)
+
+      ;; Allow up and down arrow to work for navigation
+      (setq ido-vertical-define-keys 'C-n-C-p-up-down-left-right))))
+
 
 (provide 'grass-ido)
