beb6ac77e8592ceee254c0010f01fc1412b6b12d
Ray Grasso <ray.grasso@gmail.com>
2015-11-18 12:05:01 +0800

    Some placeholder customisations

--- emacs.d/custom.el
+++ emacs.d/custom.el
@@ -4,6 +4,10 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ ;; '(helm-buffer-size ((t (:foreground "#586e75" :height 1.6))))
+ ;; '(helm-header ((t (:inherit header-line :height 1.7))))
+ ;; '(show-paren-match ((t (:underline "#d33682"))))
+ )
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
