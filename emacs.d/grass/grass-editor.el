66f12e78ac6729431862a081223d6910b79c1ee6
Ray Grasso <ray.grasso@gmail.com>
2015-12-01 10:42:38 +1100

    Make up keybinding in dired `/`

--- emacs.d/grass/grass-editor.el
+++ emacs.d/grass/grass-editor.el
@@ -282,7 +282,7 @@
     "Bunch of stuff to run for dired, either immediately or when it's loaded."
     (define-key dired-mode-map [return] 'dired-single-buffer)
     (define-key dired-mode-map [mouse-1] 'dired-single-buffer-mouse)
-    (define-key dired-mode-map "U"
+    (define-key dired-mode-map "/"
       (function
        (lambda nil (interactive) (dired-single-buffer ".."))))
     (define-key dired-mode-map "^"
