5b4cb64e7371cbac42acdb084ee3f78be952da02
Ray Grasso <ray.grasso@gmail.com>
2016-02-22 21:27:58 +1100

    Add rectangle commands hydra

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -558,6 +558,32 @@ This functions should be added to the hooks of major modes for programming."
   ("m" string-inflection-camelcase "UpperCamel")
   ("d" string-inflection-lisp "dash-case"))
 
+(defhydra hydra-rectangle (:body-pre (rectangle-mark-mode 1)
+                           :color pink
+                           :post (deactivate-mark))
+  "
+  ^_k_^     _d_elete    _s_tring
+_h_   _l_   _o_k        _y_ank
+  ^_j_^     _n_ew-copy  _r_eset
+^^^^        _e_xchange  _u_ndo
+^^^^        ^ ^         _p_aste
+"
+  ("h" backward-char nil)
+  ("l" forward-char nil)
+  ("k" previous-line nil)
+  ("j" next-line nil)
+  ("e" exchange-point-and-mark nil)
+  ("n" copy-rectangle-as-kill nil)
+  ("d" delete-rectangle nil)
+  ("r" (if (region-active-p)
+           (deactivate-mark)
+         (rectangle-mark-mode 1)) nil)
+  ("y" yank-rectangle nil)
+  ("u" undo nil)
+  ("s" string-rectangle nil)
+  ("p" kill-rectangle nil)
+  ("o" nil nil))
+(global-set-key (kbd "C-, R") 'hydra-rectangle/body)
 
 ;; Duplication of lines
 (defun grass/get-positions-of-line-or-region ()
