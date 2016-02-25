43787e4b9536c38bdc4357f5e23f9cec9ebf6900
Ray Grasso <ray.grasso@gmail.com>
2016-02-25 16:41:53 +1100

    Add rsync to dired

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -930,6 +930,37 @@ _SPC_ cancel     _o_nly this       _d_elete
     (use-package dired-rainbow)
     (dired-rainbow-define-chmod executable-unix "#4e9a06" "-.*x.*")
 
+    (defun grass/dired-rsync (dest)
+      (interactive
+       (list
+        (expand-file-name
+         (read-file-name
+          "Rsync to:"
+          (dired-dwim-target-directory)))))
+      ;; store all selected files into "files" list
+      (let ((files (dired-get-marked-files
+                    nil current-prefix-arg))
+            ;; the rsync command
+            (tmtxt/rsync-command
+             "rsync -arvz --progress "))
+        ;; add all selected file names as arguments
+        ;; to the rsync command
+        (dolist (file files)
+          (setq tmtxt/rsync-command
+                (concat tmtxt/rsync-command
+                        (shell-quote-argument file)
+                        " ")))
+        ;; append the destination
+        (setq tmtxt/rsync-command
+              (concat tmtxt/rsync-command
+                      (shell-quote-argument dest)))
+        ;; run the async shell command
+        (async-shell-command tmtxt/rsync-command "*rsync*")
+        ;; finally, switch to that window
+        (other-window 1)))
+
+    (define-key dired-mode-map "Y" 'grass/dired-rsync)
+
     ;; Reuse the same buffer for dired windows
     (use-package dired-single
       :init
