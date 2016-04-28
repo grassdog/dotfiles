461c9c08b12df62acf9a4fce8c0171b9ca0d010c
Ray Grasso <ray.grasso@gmail.com>
2016-04-28 09:41:23 +0800

    Ad `awsi` alias

--- zsh/smithy.zsh
+++ zsh/smithy.zsh
@@ -1,4 +1,6 @@
 # This should be here but for some reason when it is the PATH isn't set correctly
 # source /opt/boxen/env.sh
 
-eval $(docker-machine env default)
+#eval $(docker-machine env default)
+
+alias awsi="aws-vault exec identity-production-admin --"
