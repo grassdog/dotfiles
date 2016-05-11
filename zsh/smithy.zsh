990a5943a6e8480d88445abec847efcf15dc877f
Ray Grasso <ray.grasso@gmail.com>
2016-05-12 07:32:42 +0800

    Add GOPATH

--- zsh/smithy.zsh
+++ zsh/smithy.zsh
@@ -4,3 +4,8 @@
 #eval $(docker-machine env default)
 
 alias awsi="aws-vault exec identity-production-admin --"
+
+export GOPATH=$HOME/.golang
+export GOROOT=/usr/local/opt/go/libexec
+export PATH=$PATH:$GOPATH/bin
+export PATH=$PATH:$GOROOT/bin
