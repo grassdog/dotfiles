e9dbf781441584fdcdf7f1f86ac8a24d859a45d1
Ray Grasso <ray.grasso@gmail.com>
2016-06-19 17:25:51 +0800

    Add smithy deps

---
+++ babushka-deps/smithy.rb
@@ -0,0 +1,9 @@
+# Keep this minimal as it is mostly managed via our-boxen
+dep "smithy" do
+  requires "dotfiles-installed",
+           "moom-config.file",
+           "proselint.pip",
+           "all-fonts",
+           "colourpickers"
+end
+
