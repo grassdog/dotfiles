ae97c93f0c95cbd00a52bf3720b4dbf0a8ebc083
Ray Grasso <ray.grasso@gmail.com>
2016-08-03 11:25:40 +0800

    Add sicp info to emacs

--- files/.spacemacs.d/init.el
+++ files/.spacemacs.d/init.el
@@ -90,7 +90,7 @@ values."
    ;; wrapped in a layer. If you need some configuration for these
    ;; packages then consider to create a layer, you can also put the
    ;; configuration in `dotspacemacs/config'.
-   dotspacemacs-additional-packages '()
+   dotspacemacs-additional-packages '(sicp)
    ;; A list of packages and/or extensions that will not be install and loaded.
    dotspacemacs-excluded-packages '(emmet-mode)
    ;; If non-nil spacemacs will delete any orphan packages, i.e. packages that
