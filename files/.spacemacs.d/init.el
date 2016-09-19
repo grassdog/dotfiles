60296c45f29db312e3e5f1f5f3eddfb19e96edc3
Ray Grasso <ray.grasso@gmail.com>
2016-09-19 19:14:03 +0800

    Set eshell as default

--- files/.spacemacs.d/init.el
+++ files/.spacemacs.d/init.el
@@ -46,6 +46,7 @@ values."
       markdown
       org
       (shell :variables
+            shell-default-shell 'eshell
             shell-default-height 30
             shell-default-position 'bottom)
       (spell-checking :variables
