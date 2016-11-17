42468b3e0c61f329e4a3bb70f1ad95fbc39a3132
Ray Grasso <ray.grasso@gmail.com>
2016-11-17 11:27:51 +0800

    Add open-this-file-as-other-user

--- files/.emacs.d/init.el
+++ files/.emacs.d/init.el
@@ -1084,6 +1084,20 @@ With C-u C-u: insert time"
         (grass/indent-buffer)
         (message "Indented buffer.")))))
 
+(defun grass/open-this-file-as-other-user (user)
+  "Edit current file as USER, using `tramp' and `sudo'.  If the current
+buffer is not visiting a file, prompt for a file name."
+  (interactive "sEdit as user (default: root): ")
+  (when (string= "" user)
+    (setq user "root"))
+  (let* ((filename (or buffer-file-name
+                       (read-file-name (format "Find file (as %s): "
+                                               user))))
+         (tramp-path (concat (format "/sudo:%s@localhost:" user) filename)))
+    (if buffer-file-name
+        (find-alternate-file tramp-path)
+      (find-file tramp-path))))
+
 ;;
 ;; Buffer switching
 ;;
