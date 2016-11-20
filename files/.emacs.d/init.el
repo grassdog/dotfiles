1e8d365dff332fb9e9a55708820e348c2f143069
Ray Grasso <ray.grasso@gmail.com>
2016-11-20 15:38:21 +0800

    Remove old config cruft

--- files/.emacs.d/init.el
+++ files/.emacs.d/init.el
@@ -48,14 +48,8 @@
 ;; File paths
 (defvar grass/dotfiles-dir (file-name-directory load-file-name)
   "The root dir of my Emacs config.")
-(defvar grass/config-dir (expand-file-name "grass" grass/dotfiles-dir)
-  "The directory containing configuration files.")
-(defvar grass/snippets-dir (expand-file-name "snippets" grass/dotfiles-dir)
-  "A house for snippets.")
 (defvar grass/savefile-dir (expand-file-name "savefile" grass/dotfiles-dir)
   "This folder stores all the automatically generated save/history-files.")
-(defvar grass/undo-dir (expand-file-name "undo" grass/dotfiles-dir)
-  "Undo files.")
 
 ;; Ensure savefile directory exists
 (unless (file-exists-p grass/savefile-dir)
@@ -66,7 +60,6 @@
 (load custom-file)
 
 ;; Set up load paths
-(add-to-list 'load-path grass/config-dir)
 (add-to-list 'load-path (expand-file-name "vendor" grass/dotfiles-dir))
 
 (setq user-full-name "Ray Grasso"
