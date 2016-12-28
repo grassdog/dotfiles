133f231c3366c18a8f0997dd5592a353ec52d643
Ray Grasso <ray.grasso@gmail.com>
2016-12-28 13:24:38 +0800

    Add emoji to emacs

--- files/.emacs.d/init.el
+++ files/.emacs.d/init.el
@@ -2528,6 +2528,32 @@ the right."
     "e" 'markdown-export-and-preview))
 
 
+;;;;;;;;;;;
+;; Emoji ;;
+;;;;;;;;;;;
+
+(use-package emojify
+
+  :config
+  (progn
+    (add-hook 'org-mode-hook 'emojify-mode)
+    (add-hook 'markdown-mode-hook 'emojify-mode)))
+
+(defun --set-emoji-font (frame)
+  "Adjust the font settings of FRAME so Emacs can display emoji properly."
+  (if (eq system-type 'darwin)
+      ;; For NS/Cocoa
+      (set-fontset-font t 'symbol (font-spec :family "Apple Color Emoji") frame 'prepend)
+    ;; For Linux
+    (set-fontset-font t 'symbol (font-spec :family "Symbola") frame 'prepend)))
+
+;; For when Emacs is started in GUI mode:
+(--set-emoji-font nil)
+;; Hook for when a frame is created with emacsclient
+;; see https://www.gnu.org/software/emacs/manual/html_node/elisp/Creating-Frames.html
+(add-hook 'after-make-frame-functions '--set-emoji-font)
+
+
 ;;;;;;;;;;;;;
 ;; Haskell ;;
 ;;;;;;;;;;;;;
@@ -3276,6 +3302,7 @@ If the error list is visible, hide it.  Otherwise, show it."
   "ec" 'char-menu
   "eb" 'grass/comment-box
   "ed" 'grass/insert-datetime
+  "ee" 'emojify-insert-emoji
 
   "ea" '(:ignore t :which-key "Alignment")
   "eaa" 'align
