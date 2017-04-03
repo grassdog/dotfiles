66e06a759472028d024903212e622da5ad170451
Ray Grasso <ray.grasso@gmail.com>
2017-04-03 10:00:37 +0800

    Shorten alias

--- files/.zsh/aliases.zsh
+++ files/.zsh/aliases.zsh
@@ -207,6 +207,6 @@ function notes() {
 }
 
 # Look up SSL cert details
-function get-ssl-details() {
+function ssl-details() {
   echo | openssl s_client -connect $1:443 2>/dev/null | openssl x509 -noout -issuer -subject -dates
 }
