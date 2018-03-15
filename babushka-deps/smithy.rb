1372d2c4f6221c3f65beb7aa8ed30bf4872c0704
Ray Grasso <ray.grasso@gmail.com>
2018-03-15 20:34:31 +0800

    Remove old babushka deps

--- babushka-deps/smithy.rb
+++
@@ -1,9 +0,0 @@
-# Keep this minimal as it is mostly managed via our-boxen
-dep "smithy" do
-  requires "dotfiles-installed",
-           "moom-config.file",
-           "proselint.pip",
-           "all-fonts",
-           "colourpickers"
-end
-
