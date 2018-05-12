5dc5a224de02471fb75acd5efeb8acaaeefe4e9b
Ray Grasso <ray.grasso@gmail.com>
2018-05-12 11:31:30 +0800

    Add insert timestamp hydra

--- files/.emacs.d/init.el
+++ files/.emacs.d/init.el
@@ -1125,6 +1125,81 @@ _SPC_ cancel     _o_nly this       _d_elete
 
 (add-hook 'minibuffer-setup-hook 'grass/minibuffer-setup-hook)
 
+(defhydra hydra-insert-timestamp (:color blue :hint nil)
+  "
+Timestamps: (_q_uit)
+  Date: _I_SO, _U_S, US With _Y_ear and _D_ashes, US In _W_ords
+   Date/Time: _N_o Colons or _w_ith
+    Org-Mode: _R_ight Now or _c_hoose
+"
+  ("q" nil)
+
+  ("I" help/insert-datestamp)
+  ("U" help/insert-datestamp-us)
+  ("Y" help/insert-datestamp-us-full-year)
+  ("D" help/insert-datestamp-us-full-year-and-dashes)
+  ("W" help/insert-datestamp-us-words)
+
+  ("N" help/insert-timestamp-no-colons)
+  ("w" help/insert-timestamp)
+
+  ("R" help/org-time-stamp-with-seconds-now)
+  ("c" org-time-stamp))
+(global-set-key (kbd "C-t") #'help/hydra/timestamp/body)
+(defun help/insert-datestamp ()
+  "Produces and inserts a partial ISO 8601 format timestamp."
+  (interactive)
+  (insert (format-time-string "%F")))
+(defun help/insert-datestamp-us ()
+  "Produces and inserts a US datestamp."
+  (interactive)
+  (insert (format-time-string "%m/%d/%y")))
+(defun help/insert-datestamp-us-full-year-and-dashes ()
+  "Produces and inserts a US datestamp with full year and dashes."
+  (interactive)
+  (insert (format-time-string "%m-%d-%Y")))
+(defun help/insert-datestamp-us-full-year ()
+  "Produces and inserts a US datestamp with full year."
+  (interactive)
+  (insert (format-time-string "%m/%d/%Y")))
+(defun help/insert-datestamp-us-words ()
+  "Produces and inserts a US datestamp using words."
+  (interactive)
+  (insert (format-time-string "%A %B %d, %Y")))
+(defun help/insert-timestamp-no-colons ()
+  "Inserts a full ISO 8601 format timestamp with colons replaced by hyphens."
+  (interactive)
+  (insert (help/get-timestamp-no-colons)))
+(defun help/insert-datestamp ()
+  "Produces and inserts a partial ISO 8601 format timestamp."
+  (interactive)
+  (insert (format-time-string "%F")))
+(defun help/get-timestamp-no-colons ()
+  "Produces a full ISO 8601 format timestamp with colons replaced by hyphens."
+  (interactive)
+  (let* ((timestamp (help/get-timestamp))
+         (timestamp-no-colons (replace-regexp-in-string ":" "-" timestamp)))
+    timestamp-no-colons))
+(defun help/get-timestamp ()
+  "Produces a full ISO 8601 format timestamp."
+  (interactive)
+  (let* ((timestamp-without-timezone (format-time-string "%Y-%m-%dT%T"))
+         (timezone-name-in-numeric-form (format-time-string "%z"))
+         (timezone-utf-offset
+          (concat (substring timezone-name-in-numeric-form 0 3)
+                  ":"
+                  (substring timezone-name-in-numeric-form 3 5)))
+         (timestamp (concat timestamp-without-timezone
+                            timezone-utf-offset)))
+    timestamp))
+(defun help/insert-timestamp ()
+  "Inserts a full ISO 8601 format timestamp."
+  (interactive)
+  (insert (help/get-timestamp)))
+(defun help/org-time-stamp-with-seconds-now ()
+  (interactive)
+  (let ((current-prefix-arg '(16)))
+    (call-interactively 'org-time-stamp)))
 
 (defun grass/today ()
   (format-time-string "%Y.%m.%d - %a"))
@@ -3439,6 +3514,7 @@ If the error list is visible, hide it.  Otherwise, show it."
   "ew" 'whitespace-cleanup
   "eT" 'untabify
   "et" '(grass/toggle-always-indent :which-key "toggle tab indent")
+  "ei" 'hydra-insert-timestamp/body
   "ec" 'char-menu
   "eb" 'grass/comment-box
   "ed" 'grass/insert-date
