afe6a07413f69c33df5e473cc19256b9a3cceba3
Ray Grasso <ray.grasso@gmail.com>
2018-10-19 14:28:06 +0800

    Simplify `fe` alias

--- files/.zsh/aliases.zsh
+++ files/.zsh/aliases.zsh
@@ -248,18 +248,10 @@ function ssl-details() {
 ######
 
 # fe [FUZZY PATTERN] - Open the selected file with the default editor
-#   - Bypass fuzzy finder if there's only one match (--select-1)
-#   - Exit if there's no match (--exit-0)
-fe() {
-  local files
-  IFS=$'\n' files=($(fzf-tmux --query="$1" --multi --select-1 --exit-0))
-  [[ -n "$files" ]] && ${EDITOR:-vim} "${files[@]}"
-}
-
 # Modified version where you can press
 #   - CTRL-O to open with `open` command,
 #   - CTRL-E or Enter key to open with the $EDITOR
-fo() {
+fe() {
   local out file key
   IFS=$'\n' out=($(fzf-tmux --query="$1" --exit-0 --expect=ctrl-o,ctrl-e))
   key=$(head -1 <<< "$out")
