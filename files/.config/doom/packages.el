23cbc0627f9026c867397e82ffe6d91caeb9f11f
Ray Grasso <ray.grasso@gmail.com>
2020-11-10 15:41:36 +0800

    Try out enh-ruby-mode
    It has more sane indenting.

--- files/.config/doom/packages.el
+++ files/.config/doom/packages.el
@@ -15,6 +15,7 @@
 (package! ox-pandoc)
 (package! crux)
 (package! evil-better-visual-line)
+(package! enh-ruby-mode)
 
 ;; To install a package directly from a remote git repo, you must specify a
 ;; `:recipe'. You'll find documentation on what `:recipe' accepts here:
