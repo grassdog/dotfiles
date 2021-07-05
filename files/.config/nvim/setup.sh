3b129c78f5068cc119ed9c671e140c17d3d04d44
Ray Grasso <ray.grasso@gmail.com>
2021-07-05 09:07:05 +0800

    Make setup script idempotent

--- files/.config/nvim/setup.sh
+++ files/.config/nvim/setup.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env bash
 
-# Install Plug
-curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
+if [[ ! -f ~/.config/nvim/autoload/plug.vim ]]; then
+  echo "==> Installing Plug"
+  curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+fi
+
+echo "==> Creating backup and undo cache directories"
 mkdir -p ~/.cache/nvim/undo
 mkdir -p ~/.cache/nvim/backups
