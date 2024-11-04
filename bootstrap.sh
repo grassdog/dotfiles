be974836ba4a8aa2c56b5fe6e65d46bc14439aec
Ray Grasso <ray.grasso@gmail.com>
2024-11-04 11:07:05 +1000

    Don't bother with Elixir in asdf

--- bootstrap.sh
+++ bootstrap.sh
@@ -180,7 +180,6 @@ ok
 step "Install asdf plugins"
 asdf plugin add ruby https://github.com/asdf-vm/asdf-ruby.git
 asdf plugin add nodejs https://github.com/asdf-vm/asdf-nodejs.git
-asdf plugin add elixir https://github.com/asdf-vm/asdf-elixir.git
 asdf plugin update --all
 ok
 
